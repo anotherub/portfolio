a64e00dcac1d3e2c25d27d7510802eba6aa9070e
umeshbhat <umeshbhat30@gmail.com>
2020-08-18 12:31:27 +0530

    Updates

---
+++ precache-manifest.47ed8531cbececfc18997d8386930f04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7ed17094e20ce7ce9a680c8483756f",
+    "url": "/umeshbhat/index.html"
+  },
+  {
+    "revision": "0a6cf6ba56c90e3033e4",
+    "url": "/umeshbhat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/umeshbhat/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/umeshbhat/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6cf6ba56c90e3033e4",
+    "url": "/umeshbhat/static/js/main.e56735ff.chunk.js"
+  },
+  {
+    "revision": "05c2678d5b7ae7b5fd18",
+    "url": "/umeshbhat/static/js/runtime-main.69ded581.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/umeshbhat/static/media/logo.5d5d9eef.svg"
+  }
+]);
