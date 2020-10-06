b3d61faa6f0485a7913330bc57ae99af0cd32ef4
umeshbhat <umeshbhat30@gmail.com>
2020-10-06 11:45:25 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ec960fe7dd4b758cea3938e4d8677f3.js"
+  "/precache-manifest.7a8e56dd751aa664741ba0df78dab93f.js"
 );
 
 self.addEventListener('message', (event) => {
